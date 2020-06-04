a4ac76fd2b627610789e890372071fa39e3417a9
YanMolD <45833301+YanMolD@users.noreply.github.com>
2020-06-04 23:11:54 +0300

    Add files via upload

--- KURS_UT.cpp
+++ KURS_UT.cpp
@@ -62,5 +62,53 @@ namespace KURSUT
 				Assert::AreEqual(exception.what(), "No symbols in file");
 			}
 		}
+		TEST_METHOD(Test_exception_no_ways)
+		{
+			ofstream input_file_example("Text_input.txt");
+			input_file_example.clear();
+			input_file_example << "S;O;3\nS;P;3\nO;P;2\nQ;R;4\nR;T;2\nQ;T;3";
+			input_file_example.close();
+			ifstream input_file("Text_input.txt");
+			try
+			{
+				read_file(input_file);
+			}
+			catch (invalid_argument exception)
+			{
+				Assert::AreEqual(exception.what(), "In this net doesn't exist way from source to sink");
+			}
+		}
+		TEST_METHOD(Test_exception_negative_data)
+		{
+			ofstream input_file_example("Text_input.txt");
+			input_file_example.clear();
+			input_file_example << "S;V;-5\nS;I;-3\nV;I;-2\nF;T;5\nG;T;2\nI;K;1";
+			input_file_example.close();
+			ifstream input_file("Text_input.txt");
+			try
+			{
+				read_file(input_file);
+			}
+			catch (invalid_argument exception)
+			{
+				Assert::AreEqual(exception.what(), "Incorrect data in file");
+			}
+		}
+		TEST_METHOD(Test_exception_loop)
+		{
+			ofstream input_file_example("Text_input.txt");
+			input_file_example.clear();
+			input_file_example << "S;V;5\nS;I;3\nV;I;2\nV;G;3\nI;F;2\nG;F;4\nF;T;5\nG;T;2\nI;K;1\nK;F;1\nK;I;1";
+			input_file_example.close();
+			ifstream input_file("Text_input.txt");
+			try
+			{
+				read_file(input_file);
+			}
+			catch (invalid_argument exception)
+			{
+				Assert::AreEqual(exception.what(), "In this net exist a loop");
+			}
+		}
 	};
 }
